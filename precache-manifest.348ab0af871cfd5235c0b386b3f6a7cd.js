399e90ca3cc77764d57f148c6db6fcf7c6b19f7c
Esteban Bojorquez <estebanb_7@hotmail.com>
2020-11-17 01:31:04 -0600

    Updates

---
+++ precache-manifest.348ab0af871cfd5235c0b386b3f6a7cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f70bb2909e9f0b908734",
+    "url": "/appi-uag-b/static/js/runtime~main.5e7f515a.js"
+  },
+  {
+    "revision": "d8f223fc33569211c563",
+    "url": "/appi-uag-b/static/js/main.ed452620.chunk.js"
+  },
+  {
+    "revision": "91a7c7b3a8810d40dd8a",
+    "url": "/appi-uag-b/static/js/2.2b4b6ed3.chunk.js"
+  },
+  {
+    "revision": "d8f223fc33569211c563",
+    "url": "/appi-uag-b/static/css/main.0097d2bf.chunk.css"
+  },
+  {
+    "revision": "418cfaa71a1d81b8002b3cc451a4c4c7",
+    "url": "/appi-uag-b/index.html"
+  }
+];
